ee936a9179d133faf54f37dd9c2c0fcf53f484fe
patycorona <pchdz.mxtobn@gmail.com>
2023-12-13 13:17:06 -0600

    útlmos cambios

--- app/src/main/java/com/example/movientf/domain/model/ConstantGeneral.kt
+++ app/src/main/java/com/example/movientf/domain/model/ConstantGeneral.kt
@@ -7,7 +7,7 @@ class ConstantGeneral {
         const val ZERO = 0
         const val ONE = 1
         const val TWO = 2
-        const val THREE = 1
+        const val THREE = 3
         const val CODE_OK = "0"
         const val URI_RESOURCE = "android.resource://"
         const val VIEW_PAGER_NUM = 4
--- app/src/main/java/com/example/movientf/ui/presentation/views/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/movientf/ui/presentation/views/adapter/ViewPagerAdapter.kt
@@ -1,11 +1,11 @@
 package com.example.movientf.ui.presentation.views.adapter
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.movientf.domain.model.ConstantGeneral.Companion.ARG_OBJECT
 import com.example.movientf.domain.model.ConstantGeneral.Companion.ONE
+import com.example.movientf.domain.model.ConstantGeneral.Companion.THREE
+import com.example.movientf.domain.model.ConstantGeneral.Companion.TWO
 import com.example.movientf.domain.model.ConstantGeneral.Companion.VIEW_PAGER_NUM
 import com.example.movientf.ui.presentation.views.VPFourFragment
 import com.example.movientf.ui.presentation.views.VPOneFragment
@@ -17,15 +17,11 @@ class ViewPagerAdapter (fa : FragmentActivity) : FragmentStateAdapter(fa) {
     override fun getItemCount(): Int  = VIEW_PAGER_NUM
 
     override fun createFragment(position: Int): Fragment {
-//       val fragment = VPOneFragment()
-//        fragment.arguments = Bundle().apply {
-//            putInt(ARG_OBJECT,position + ONE)
-//        }
-//        return fragment
+
         return when(position){
-            1 -> { VPTwoFragment() }
-            2 -> { VPThreeFragment() }
-            3 -> { VPFourFragment() }
+            ONE -> { VPTwoFragment() }
+            TWO -> { VPThreeFragment() }
+            THREE -> { VPFourFragment() }
             else -> VPOneFragment()
         }
 
